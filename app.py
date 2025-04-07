dc243d5b0c08e928f7de199addb88a5ae9b65c59
piyawatvinm <67508605+piyawatvinm@users.noreply.github.com>
2025-04-07 16:49:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ try:
     st.title('Gemini Pro Test')
 
   def role_to_steamlit(role:str) -> str:
-    if role == 'model':
-      return 'assistant'
+    if role == "model":
+      return "assistant"
     else: 
-    return role
+      return role
 
-for message in st.session_state.chat.history:
-  with st.chat_message(role_to_streamlit(message.role)):
-    st.markdown(message.part[0].text)
+  for message in st.session_state.chat.history:
+    with st.chat_message(role_to_streamlit(message.role)):
+      st.markdown(message.part[0].text)
 
-if prompt := st.chat_input("Text Here"):
-  st.chat_message('user').markdown(prompt)
-  response = st.session_state.chat.send_message(prompt)
-  with st.chat_message('assistant'):
-    st.markdown(response.text)
+  if prompt := st.chat_input("Text Here"):
+    st.chat_message('user').markdown(prompt)
+    response = st.session_state.chat.send_message(prompt)
+    with st.chat_message('assistant'):
+      st.markdown(response.text)
 except Exception as e:
   st.error(f'An error occurred{e}')
